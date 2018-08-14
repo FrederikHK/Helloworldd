58676232e5ca90ba4fe314037d7346f4766d7d81
Frederik Koch <frederikhkoch@gmail.com>
2018-08-14 13:01:13 +0200

    Switchcase

---
+++ Hellomundo/Hellomundo/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hellomundo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Vælg 1 eller 2");
+            int svar = Convert.ToInt32(Console.ReadLine());
+
+            switch (svar)
+            {
+                case 1:
+                    Console.WriteLine("Du har valgt 1");
+                    break;
+                case 2:
+                    Console.WriteLine("Du har valgt 2");
+                    break;
+            }
+
+            Console.Read();
+        }
+    }
+}
